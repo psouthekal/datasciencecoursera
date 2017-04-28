6eb6df2cb51edaddbed3acb1f07ef8688ddd0d2b
psouthekal <psouthekal@dbp-institute.com>
2017-04-28 10:18:35 -0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+library(data.table)
+## unzip file  from the working directory
+unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+
+## test data:
+XTest<- read.table("UCI HAR Dataset/test/X_test.txt")
+YTest<- read.table("UCI HAR Dataset/test/Y_test.txt")
+SubjectTest <-read.table("UCI HAR Dataset/test/subject_test.txt")
+
+## train data:
+XTrain<- read.table("UCI HAR Dataset/train/X_train.txt")
+YTrain<- read.table("UCI HAR Dataset/train/Y_train.txt")
+SubjectTrain <-read.table("UCI HAR Dataset/train/subject_train.txt")
+
+## features and activity
+features<-read.table("UCI HAR Dataset/features.txt")
+activity<-read.table("UCI HAR Dataset/activity_labels.txt")
+
+##Part1 - merge train and test data in one dataset 
+X<-rbind(XTest, XTrain)
+Y<-rbind(YTest, YTrain)
+Subject<-rbind(SubjectTest, SubjectTrain)
+
+## Part2 - Extracts only the measurements on the mean and standard deviation for each measurement
+index<-grep("mean\\(\\)|std\\(\\)", features[,2]) ##getting features indeces which contain mean() and std() in their name
+length(index) ## count of features
+
+
+## Part3 -Uses descriptive activity names to name the activities in the data set
+Y[,1]<-activity[Y[,1],2] ## replacing numeric values with lookup value from activity.txt; won't reorder Y set
+head(Y)
+
+
+## Part4 - Appropriately labels the data set with descriptive variable names.
+names<-features[index,2] ## getting names for variables
+
+names(X)<-names ## updating colNames for new dataset
+names(Subject)<-"SubjectID"
+names(Y)<-"Activity"
+
+CleanedData<-cbind(Subject, Y, X)
+head(CleanedData[,c(1:4)]) ## first 5 columns
+
+## Part5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+CleanedData<-data.table(CleanedData)
+TidyData <- CleanedData[, lapply(.SD, mean), by = 'SubjectID,Activity'] ## features average by Subject and by activity
+dim(TidyData)
+
+write.table(TidyData, file = "Tidy.txt", row.names = FALSE)
+
+## First 15 rows and 5 columns in Tidy dataset:
+head(TidyData[order(SubjectID)][,c(1:4), with = FALSE],15) 
